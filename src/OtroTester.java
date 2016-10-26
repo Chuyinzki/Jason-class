9b32131cc2099b1f41d5d8d2f11caeb482ac2416
Jesus Villegas <jesus_villegas@intuit.com>
2016-10-26 15:27:53 -0700

    Removing unnecessary array

--- src/OtroTester.java
+++ src/OtroTester.java
@@ -3,10 +3,8 @@
  */
 public class OtroTester {
 
-    int[] in = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
-
     public static void main(String[] args) {
-        int[] cube = Otro.generateCube(3);
+        int[] cube = Otro.generateCube(4);
         Otro.printCube(cube);
         Otro.printCube(Otro.getSum(cube));
     }
