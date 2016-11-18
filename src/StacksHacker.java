01caaee94771fce4b7b64a0378d88f25d7590e92
Jesus Villegas <jesus_villegas@intuit.com>
2016-11-18 12:33:20 -0800

    more solutions

--- src/StacksHacker.java
+++ src/StacksHacker.java
@@ -7,7 +7,7 @@ import java.util.Stack;
 public class StacksHacker {
 
     public static void main(String[] args) {
-        stacks3();
+        stacks4();
     }
 
     private static void stacks1() {
@@ -61,7 +61,7 @@ public class StacksHacker {
         int n1 = in.nextInt();
         int n2 = in.nextInt();
         int n3 = in.nextInt();
-        if(n1 == 0 || n2 == 0 || n3 == 0) {
+        if (n1 == 0 || n2 == 0 || n3 == 0) {
             System.out.print(0);
             return;
         }
@@ -80,15 +80,11 @@ public class StacksHacker {
         }
         for (int i = 0; i < n2; i++) {
             int temp = in.nextInt();
-            if(s2Size >= s1Size)
-                continue;
             s2Size += temp;
             temp2[i] = temp;
         }
         for (int i = 0; i < n3; i++) {
             int temp = in.nextInt();
-            if(s3Size >= s1Size)
-                continue;
             s3Size += temp;
             temp3[i] = temp;
         }
@@ -99,22 +95,22 @@ public class StacksHacker {
         }
 
         Stack<Integer> s1 = new Stack<Integer>();
-        for(int i = temp1.length - 1; i >= 0; i--)
+        for (int i = temp1.length - 1; i >= 0; i--)
             s1.push(temp1[i]);
 
         int final2Size = 0;
         Stack<Integer> s2 = new Stack<Integer>();
-        for(int i = temp2.length - 1; i >= 0; i--) {
+        for (int i = temp2.length - 1; i >= 0; i--) {
             int woink = temp2[i];
             s2.push(woink);
             final2Size += woink;
-            if(final2Size >= s1Size)
+            if (final2Size >= s1Size)
                 break;
         }
 
         int final3Size = 0;
         Stack<Integer> s3 = new Stack<Integer>();
-        for(int i = temp3.length - 1; i >= 0; i--) {
+        for (int i = temp3.length - 1; i >= 0; i--) {
             int woink = temp3[i];
             s3.push(woink);
             final3Size += woink;
@@ -139,5 +135,28 @@ public class StacksHacker {
                     final3Size -= s3.pop();
         }
         System.out.print(0);
+    }
+
+    private static void stacks4() {
+        Scanner in = new Scanner(System.in);
+        int instructionCount = in.nextInt();
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+        while (instructionCount-- > 0) {
+            int instruction = in.nextInt();
+            if (instruction == 1) {
+                s1.push(in.nextInt());
+            } else if (instruction == 2) {
+                if (s2.empty())
+                    while (!s1.empty())
+                        s2.push(s1.pop());
+                s2.pop();
+            } else {
+                if (s2.empty())
+                    while (!s1.empty())
+                        s2.push(s1.pop());
+                System.out.println(s2.peek());
+            }
+        }
     }
 }
