8a3603553ac1d83f682c84315ad06454a157e397
Jesus Villegas <jesus_villegas@intuit.com>
2016-12-12 09:50:31 -0800

    All Passing

--- src/TreeTrieHacker.java
+++ src/TreeTrieHacker.java
@@ -1,13 +1,14 @@
-import java.util.LinkedList;
-import java.util.Scanner;
+import com.sun.tools.javac.util.Pair;
+
+import java.util.*;
 
 /**
  * Created by jvillegas on 12/1/16.
  */
-public class TrieHacker {
+public class TreeTrieHacker {
 
     public static void main(String[] args) {
-        trie2();
+        tree1();
     }
 
     private static void trie1() {
@@ -114,6 +115,88 @@ public class TrieHacker {
         @Override
         public boolean equals(Object obj) {
             return this.data == ((TrieNode) obj).data;
+        }
+    }
+
+    public static void tree1() {
+        Scanner in = new Scanner(System.in);
+        LinkedList<Node> nodesToDefine = new LinkedList<>();
+        Node root = new Node(1);
+        nodesToDefine.push(root);
+        int addCount = in.nextInt();
+
+        while(addCount-- > 0) {
+            Node parent = nodesToDefine.pollFirst();
+            Node leftChild = null;
+            Node rightChild = null;
+            int leftInt = in.nextInt();
+            int rightInt = in.nextInt();
+            if(leftInt != -1) {
+                leftChild = new Node(leftInt);
+                nodesToDefine.add(leftChild);
+            }
+            if(rightInt != -1) {
+                rightChild = new Node(rightInt);
+                nodesToDefine.add(rightChild);
+            }
+            parent.left = leftChild;
+            parent.right = rightChild;
+        }
+
+        int iterationCount = in.nextInt();
+        while(iterationCount-- > 0) {
+            int depthChange = in.nextInt();
+            int mult = 1;
+            LinkedList<Node> list = getNodesAtLevel(root, depthChange);
+            while(!list.isEmpty()) {
+                for (Node node : list) {
+                    Node temp = node.left;
+                    node.left = node.right;
+                    node.right = temp;
+                }
+                list = getNodesAtLevel(root, depthChange * (++mult));
+            }
+            preOrderPrint(root);
+            System.out.println();
+        }
+    }
+
+    private static void preOrderPrint(Node root) {
+        if(root == null)
+            return;
+        preOrderPrint(root.left);
+        System.out.print(root.data + " ");
+        preOrderPrint(root.right);
+    }
+
+    private static LinkedList<Node> getNodesAtLevel(Node root, int level) {
+        LinkedList<AbstractMap.SimpleImmutableEntry<Node, Integer>> list = new LinkedList<>();
+        LinkedList<Node> retList = new LinkedList<>();
+        int cur = 1;
+        list.add(new AbstractMap.SimpleImmutableEntry<>(root, 1));
+        while(!list.isEmpty()) {
+            AbstractMap.SimpleImmutableEntry<Node, Integer> current = list.removeFirst();
+            if(current.getValue() != cur)
+                cur = current.getValue();
+
+            if(cur == level) {
+                retList.add(current.getKey());
+                continue;
+            }
+            if(current.getKey().left != null)
+                list.add(new AbstractMap.SimpleImmutableEntry<>(current.getKey().left, current.getValue() + 1));
+            if(current.getKey().right != null)
+                list.add(new AbstractMap.SimpleImmutableEntry<>(current.getKey().right, current.getValue() + 1));
+        }
+        return retList;
+    }
+
+    public static class Node {
+        Integer data;
+        Node left;
+        Node right;
+        public Node(int i){
+            data = i;
         }
     }
 }
