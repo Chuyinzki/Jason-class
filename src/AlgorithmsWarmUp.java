54cc01d4d8a6756ad759e18db6beabb78b188397
Jesus Villegas <jesus_villegas@intuit.com>
2016-12-14 12:59:38 -0800

    Working

--- src/AlgorithmsWarmUp.java
+++ src/AlgorithmsWarmUp.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class AlgorithmsWarmUp {
 
     public static void main(String[] args) {
-        diagonalDifference();
+        staircase();
     }
 
-    public static void bigSum() {
+    private static void bigSum() {
         Scanner in = new Scanner(System.in);
         int lines = in.nextInt();
         long sum = 0L;
@@ -27,14 +27,47 @@ public class AlgorithmsWarmUp {
                 cube[j][i] = in.nextInt();
 
         int primary = 0;
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             primary += cube[i][i];
 
         int secondary = 0;
         int y = n - 1;
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             secondary += cube[i][y--];
 
         System.out.print(Math.abs(primary - secondary));
+    }
+
+    private static void plusMinus() {
+        Scanner in = new Scanner(System.in);
+        int totalCount = in.nextInt();
+        int posTotal = 0;
+        int negTotal = 0;
+        int zeroTotal = 0;
+        for (int i = 0; i < totalCount; i++) {
+            int num = in.nextInt();
+            if (num > 0)
+                posTotal++;
+            else if (num < 0)
+                negTotal++;
+            else
+                zeroTotal++;
+        }
+
+        System.out.printf("%.6f\n", ((float) posTotal) / ((float) totalCount));
+        System.out.printf("%.6f\n", ((float) negTotal) / ((float) totalCount));
+        System.out.printf("%.6f\n", ((float) zeroTotal) / ((float) totalCount));
+    }
+
+    private static void staircase() {
+        Scanner in = new Scanner(System.in);
+        int stairSize = in.nextInt();
+        for(int i = stairSize - 1; i >= 0; i--) {
+            for (int j = i; j > 0; j--)
+                System.out.print(' ');
+            for (int k = stairSize - i; k > 0; k--)
+                System.out.print('#');
+            System.out.println();
+        }
     }
 }
